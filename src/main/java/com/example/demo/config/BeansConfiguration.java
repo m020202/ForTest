c4f867345778af9d7d50e782ba8d39088087cb63
m020202 <m020202@naver.com>
2024-11-01 20:38:32 +0900

    feat: CoffeeBeans 구현체 변경

--- src/main/java/com/example/demo/config/BeansConfiguration.java
+++ src/main/java/com/example/demo/config/BeansConfiguration.java
@@ -2,6 +2,7 @@ package com.example.demo.config;
 
 import com.example.demo.coffee.CoffeeBeans;
 import com.example.demo.coffee.DecafCoffeeBeans;
+import com.example.demo.coffee.RegularCoffeeBeans;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,6 @@ import org.springframework.context.annotation.Configuration;
 public class BeansConfiguration {
     @Bean
     public CoffeeBeans coffeeBeans() {
-        return new DecafCoffeeBeans();
+        return new RegularCoffeeBeans();
     }
 }
