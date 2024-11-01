0682f538cbac30cbac5c17e8d425a1eaf0b4e4e2
m020202 <m020202@naver.com>
2024-11-01 20:37:10 +0900

    feat: DI & bean EG

--- src/main/java/com/example/demo/ForTestApplication.java
+++ src/main/java/com/example/demo/ForTestApplication.java
@@ -8,11 +8,16 @@ import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class ForTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForTestApplication.class, args);
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MachineConfiguration.class);
+		CoffeeMachine machine = context.getBean(CoffeeMachine.class);
+		machine.print();
+
 	}
 }
