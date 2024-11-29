dbeff966e7eb5a5a175b0107582494e1d4ae61d4
m020202 <m020202@naver.com>
2024-11-29 13:33:20 +0900

    Fix: @req 대신 직접 작성

--- src/main/java/com/example/demo/service/AnimalService.java
+++ src/main/java/com/example/demo/service/AnimalService.java
@@ -1,18 +1,22 @@
 package com.example.demo.service;
 
 import com.example.demo.domain.animal.Animal;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class AnimalService {
-    @Qualifier("dog")
-    private Animal animal;
+
+    private final Animal animal;
+
+    public AnimalService(@Qualifier("dog") Animal animal) {
+        this.animal = animal;
+    }
 
     public String animalSound() {
         return animal.sound();
     }
-
 }
--- src/test/java/com/example/demo/service/AnimalServiceTest.java
+++ src/test/java/com/example/demo/service/AnimalServiceTest.java
@@ -7,27 +7,25 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class AnimalServiceTest {
-
-    @Mock
-    @Qualifier("dog")
-    private Animal mockAnimal;
-    @InjectMocks
+    @Autowired
     private AnimalService animalService;
 
     @Test
     void testAnimalSound() {
-       String sound = animalService.animalSound();
-
-        Assertions.assertThat(sound).isEqualTo(mockAnimal.sound());
+        String sound = animalService.animalSound();
+        System.out.println(sound);
+        //Assertions.assertThat(sound).isEqualTo("Bark");
     }
 
 }
