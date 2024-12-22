c567acb834af3673262a84ad97b1ccdae73f4139
m020202 <m020202@naver.com>
2024-12-22 15:58:53 +0900

    Feat: 로그인 테스트 코드 구현

--- src/main/java/com/example/demo/controller/JoinController.java
+++ src/main/java/com/example/demo/controller/JoinController.java
@@ -1,10 +1,23 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.converter.MemberConverter;
+import com.example.demo.dto.JoinDTO;
+import com.example.demo.service.JoinService;
+import com.example.demo.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class JoinController {
+    private final JoinService joinService;
 
-
+    @PostMapping("/join")
+    public String join(@RequestBody JoinDTO joinDTO) {
+        joinService.join(joinDTO);
+        return "회원가입 성공!";
+    }
 }
--- src/main/java/com/example/demo/filter/CustomUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/example/demo/filter/CustomUsernamePasswordAuthenticationFilter.java
@@ -29,11 +29,12 @@ public class CustomUsernamePasswordAuthenticationFilter extends UsernamePassword
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+        System.out.println("로그인 성공!");
+
     }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
-        super.unsuccessfulAuthentication(request, response, failed);
+        System.out.println("로그인 실패!");
     }
 }
