be02606705255231534762313804a7d3443f13fb
m020202 <m020202@naver.com>
2024-12-23 15:53:58 +0900

    Feat: SecurityContext 세션 정보 확인

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,14 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 public class MainController {
 
     @GetMapping("/")
     public String mainP() {
-        return "Main Controller";
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        String role = SecurityContextHolder.getContext().getAuthentication().getAuthorities().iterator().next().getAuthority();
+        return name + "님 (" + role + ") 환영합니다!";
     }
+
 }
