835640929624787cfca639adfb0844f32e2eaee0
m020202 <m020202@naver.com>
2024-12-23 13:18:31 +0900

    Feat: 로그인 후 어드민 접속 테스트

--- src/main/java/com/example/demo/controller/JoinController.java
+++ src/main/java/com/example/demo/controller/JoinController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.dto.JoinDTO;
 import com.example.demo.service.JoinService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +17,10 @@ public class JoinController {
     public String join(@RequestBody JoinDTO joinDTO) {
         joinService.join(joinDTO);
         return "회원가입 성공!";
+    }
+
+    @GetMapping
+    public String admin() {
+        return "admin 페이지입니다!";
     }
 }
