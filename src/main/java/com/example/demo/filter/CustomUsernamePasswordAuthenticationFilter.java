c482055692ce5d635dc39bc223637fa29c992ef3
m020202 <m020202@naver.com>
2024-12-23 12:59:41 +0900

    Feat: 로그인 성공 후 토큰 생성 및 발급 구현

--- src/main/java/com/example/demo/filter/CustomUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/example/demo/filter/CustomUsernamePasswordAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.example.demo.filter;
 
+import com.example.demo.jwt.CustomUserDetails;
 import com.example.demo.jwt.JWTUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -10,9 +11,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
 
 @RequiredArgsConstructor
 public class CustomUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -30,9 +34,16 @@ public class CustomUsernamePasswordAuthenticationFilter extends UsernamePassword
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        System.out.println("로그인 성공!");
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+        String name = customUserDetails.getUsername();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
 
+        String token = jwtUtil.createJwt(name, role, 60 * 60 * 10L);
+        response.addHeader("Authorization", "Bearer " + token);
     }
 
     @Override
