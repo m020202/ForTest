b188bac72d1328a7fcb07349974bc0f1b8155ec7
m020202 <m020202@naver.com>
2024-12-30 20:05:40 +0900

    Feat: access / refresh 두 개 토큰 발급하도록 구현

--- src/main/java/com/example/demo/jwt/filter/CustomUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/example/demo/jwt/filter/CustomUsernamePasswordAuthenticationFilter.java
@@ -4,10 +4,12 @@ import com.example.demo.jwt.util.CustomUserDetails;
 import com.example.demo.jwt.util.JWTUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -17,8 +19,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import java.io.IOException;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 @RequiredArgsConstructor
 public class CustomUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -39,15 +43,20 @@ public class CustomUsernamePasswordAuthenticationFilter extends UsernamePassword
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
         // JWT 생성 및 응답 헤더에 세팅
-        String token = "Bearer " + createToken(authentication);
-        response.addHeader("Authorization", token);
+        List<String> tokens = createToken(authentication);
+        String accessToken = tokens.get(0);
+        String refreshToken = tokens.get(1);
+
+        response.setHeader("Authorization", accessToken);
+        response.addCookie(createCookie("Refresh", refreshToken));
+        response.setStatus(HttpStatus.OK.value());
 
         // 생성된 토큰 redis에 저장
-        redisTemplate.opsForValue().set(authentication.getName(), token, Duration.ofSeconds(60 * 60 * 10L));
+        redisTemplate.opsForValue().set(authentication.getName(), accessToken, Duration.ofSeconds(60 * 60 * 10L));
 
     }
 
-    private String createToken(Authentication authentication) {
+    private List<String> createToken(Authentication authentication) {
         CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
         String name = customUserDetails.getUsername();
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
@@ -55,7 +64,20 @@ public class CustomUsernamePasswordAuthenticationFilter extends UsernamePassword
         GrantedAuthority auth = iterator.next();
         String role = auth.getAuthority();
 
-        return jwtUtil.createJwt(name, role, 60 * 60 * 10L);
+        String access = jwtUtil.createJwt("access", name, role, 600000L);
+        String refresh = jwtUtil.createJwt("refresh", name, role, 86400000L);
+        List<String> list = new ArrayList<>();
+        list.add(access);
+        list.add(refresh);
+        return list;
+    }
+
+    private Cookie createCookie(String key, String value) {
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(24 * 60 * 60);
+        cookie.setHttpOnly(true);
+
+        return cookie;
     }
 
     @Override
