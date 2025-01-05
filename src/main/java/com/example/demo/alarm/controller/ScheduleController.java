16b52c8919bc66fcd469686da2f1b2a369971ba0
m020202 <m020202@naver.com>
2025-01-05 21:05:03 +0900

    Feat: 특정 요일, 시간에 해당하는 일정 조회 로직 구현

--- src/main/java/com/example/demo/alarm/controller/ScheduleController.java
+++ src/main/java/com/example/demo/alarm/controller/ScheduleController.java
@@ -1,12 +1,18 @@
 package com.example.demo.alarm.controller;
 
+import com.example.demo.alarm.domain.Schedule;
 import com.example.demo.alarm.dto.ScheduleRequestDTO;
 import com.example.demo.alarm.service.ScheduleService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +24,11 @@ public class ScheduleController {
         scheduleService.create(dto);
 
         return "OK!";
+    }
+
+    @GetMapping("/schedules")
+    public List<Schedule> getSchedules() {
+        List<Schedule> schedules = scheduleService.findByDayAndTime(DayOfWeek.MONDAY, LocalTime.of(16, 0, 0));
+        return schedules;
     }
 }
--- src/main/java/com/example/demo/alarm/repository/ScheduleRepository.java
+++ src/main/java/com/example/demo/alarm/repository/ScheduleRepository.java
@@ -1,12 +1,20 @@
 package com.example.demo.alarm.repository;
 
 import com.example.demo.alarm.domain.Schedule;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
 import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-//    @Query("")
-//    public List<Schedule> findByDayAndTime
+    @Query(value = "SELECT s.* FROM schedule s " +
+            "JOIN schedule_repeat_day d ON s.id = d.schedule_id " +
+            "WHERE d.repeat_days = :day AND s.time = :time",
+            nativeQuery = true
+    )
+    List<Schedule> findByDayAndTime(@Param("day") String day, @Param("time") LocalTime time);
+
 }
